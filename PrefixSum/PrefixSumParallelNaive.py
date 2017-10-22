cd78b52754605c3223faada502428c12d47b3ddf
dshpet <dshpet@gmail.com>
2017-10-22 20:04:22 +0300

    sanitize output, remove comments

--- PrefixSum/PrefixSumParallelNaive.py
+++ PrefixSum/PrefixSumParallelNaive.py
@@ -7,7 +7,6 @@ http://people.cs.vt.edu/yongcao/teaching/cs5234/spring2013/slides/Lecture10.pdf
 
 import threading
 import math
-# look maybe multiprocessing lib
 
 # test_data = [2,6,2,3,5]
 test_data = [3, 1, 7, 0, 4, 1, 6, 3]
@@ -15,7 +14,7 @@ test_data = [3, 1, 7, 0, 4, 1, 6, 3]
 '''
 Generic sum function 
 '''
-def accumulate(in_list, index, stride, out_list, out_index): # todo slightly refactor
+def accumulate(in_list, index, stride, out_list, out_index):
 	sum = in_list[index] + in_list[index - stride]
 
 	out_list[out_index] = sum
@@ -31,13 +30,13 @@ def prefixSum(num_list):
 
 	size = len(num_list)
 	iterations = int(math.log(size, 2)) # not sure if base 2
-	print str(iterations)
+	print(str(iterations))
 	stride = 1
 
 	for iteration in range(0, iterations):
 		jobs = []
 		for i in range(stride, size):
-			thread = threading.Thread(target = accumulate(current_list, i, stride, out, i)) # todo slightly refactor
+			thread = threading.Thread(target = accumulate(current_list, i, stride, out, i))
 			jobs.append(thread)
 
 		for job in jobs:
@@ -48,7 +47,7 @@ def prefixSum(num_list):
 
 		stride *= 2
 		current_list = list(out)
-		print out
+		print(out)
 
 	return out
 
